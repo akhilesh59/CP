10e89212543a82307a6a5c4ef781100424aee50e
Akhilesh Singh <akhisingh6871@gmail.com>
2022-05-28 11:45:48 +0530

    Updated Template

--- z----------------template----------------z.cpp
+++ z----------------template----------------z.cpp
@@ -3,9 +3,7 @@
     User Handle : akhilesh59
     Disclaimer : Please do not copy my code, it will lead to Plagarism.
 
-    Status : 
-
-    'Abe Padhai likhai pe dhyaan do, IAS-YAS bano aur desh ko sambhalo. Lekin nhi, tumko to SDE banna hai!'
+    'Abe Padhai likhai pe dhyaan do, IAS-YAS bano aur desh ko sambhalo'
 */
 
 #include <bits/stdc++.h>
@@ -67,6 +65,19 @@ vector<ll> sieveOfEratosthenes(ll n) { vector<ll> arr(n+1); for(ll i = 2; i <= n
 
 vector<ll> prFactUsingSieve(ll n) { vector<ll> arr(n+1); vector<ll> ans; for (ll i = 0; i <= n; i++) arr[i] = i; for (ll i = 2; i <= n; i++) { if(arr[i] == i) { for(ll j=i*i; j<=n; j+=i) { if(arr[j] == j) {arr[j] = i;} } } } while(n != 1) { /*cout<<arr[n]<<" ";*/ ans.push_back(arr[n]); n /= arr[n]; } return ans; }
 
+//--------------------------------- Modulo Operations -----------------------------------
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+
 //--------------------------- Euclid's Algo ---------------------------------------------------
 
 ll gcd(ll a, ll b) { ll rem; while(b) { rem = a%b; a = b; b = rem; } return a; }
@@ -85,18 +96,21 @@ ll binpow(ll a, ll b, ll m) {a %= m; ll res = 1; while (b > 0) {if (b & 1) res =
 
 //------------------------------------------------------- Macros -----------------------------------------
 
-#define         vi vector<int>
+#define         F first
+#define         S second
 #define         vl vector<ll>
 #define         umpll unordered_map<ll, ll>
+#define         mpll map<ll, ll>
 #define         umpsl unordered_map<string, ll>
 #define         mkpr make_pair
-#define         Fien for (int i = 0; i <= n; i++)
-#define         Fln for (ll i = 0; i < n; i++)
+#define         forl(s,e,f) for (ll i = s; i < n; i+=f)
 #define         pb push_back
 #define         complete(v) v.begin(), v.end()
 #define         itr_v for (auto it = v.begin(); it != v.end; it++)
 #define         sort_asc(v) sort(v.begin(), v.end())
 #define         sort_dec(v) sort(v.begin(), v.end(), greater<int>())
+#define         accu(v) accumulate(v.begin(), v.end(), 0LL)
+#define         accu_range(v,x,y) accumulate(v.begin()+x, v.end()-y, 0LL)
 #define         deb(x) cout << #x << "=" << x << endl
 #define         deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 
@@ -121,22 +135,27 @@ int main()
 }
 
 /*
-*
-*　　┏┓　　　┏┓+ +
-*　┏┛┻━━━┛┻┓ + +
-*　┃　　　　　　　┃
-*　┃　　　━　　　┃ ++ + + +
-*  +████━████++
-*  ◥██◤　◥██◤ +
-*　┃　　　┻　　　┃
-*　┃　　　　　　　┃ + +
-*　┗━┓　　　┏━┛
-*　　　┃　　　┃ + + + + 
-*　　　┃　　　┃ + Beware of the Dog
-*　　　┃　 　 ┗━━━┓ 　
-*　　　┃ 　　　　　　 ┣┓
-*　　  ┃ 　　　　　 　┏┛
-*　    ┗┓┓┏━┳┓┏┛ + + + +
-*　　　　┃┫┫　┃┫┫
-*　　　　┗┻┛　┗┻┛+ + + +
+
+                                        _¶¶_______________¶¶
+                                        _¶¶_______¶¶¶¶¶¶¶¶¶¶
+                                        __¶¶_____¶¶¶¶¶_¶¶¶¶_
+                                        __¶¶__¶__¶¶___¶_____
+                                        _¶¶¶_¶¶¶_¶¶¶_¶¶¶____
+                                        _¶¶¶_____¶¶¶________
+                                        _¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_
+                                        __¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_
+                                        __________¶¶_____¶¶_
+                                        _____¶¶¶__¶¶_¶¶¶_¶¶_
+                                        ______¶___¶¶__¶__¶¶_
+                                        __¶¶¶¶¶¶¶¶¶¶_____¶¶_
+                                        ¶¶¶¶____¶¶________¶¶
+
+                    ──╔╗╔═══╗╔══╗     ╔═══╗╔╗─╔╗╔═══╗╔═══╗╔═══╗     ╔═══╗╔═══╗╔═╗╔═╗   
+                    ──║║║╔═╗║╚╣─╝     ║╔═╗║║║─║║║╔═╗║║╔══╝║╔══╝     ║╔═╗║║╔═╗║║║╚╝║║
+                    ──║║║║─║║─║║─     ║╚══╗║╚═╝║║╚═╝║║╚══╗║╚══╗     ║╚═╝║║║─║║║╔╗╔╗║
+                    ╔╗║║║╚═╝║─║║─     ╚══╗║║╔═╗║║╔╗╔╝║╔══╝║╔══╝     ║╔╗╔╝║╚═╝║║║║║║║
+                    ║╚╝║║╔═╗║╔╣─╗     ║╚═╝║║║─║║║║║╚╗║╚══╗║╚══╗     ║║║╚╗║╔═╗║║║║║║║
+                    ╚══╝╚╝─╚╝╚══╝     ╚═══╝╚╝─╚╝╚╝╚═╝╚═══╝╚═══╝     ╚╝╚═╝╚╝─╚╝╚╝╚╝╚╝                                        
+
+
 */
